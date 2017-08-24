38eec9f305d7fe46bef7e683e139d62acac16d96
marckom <marco.el.loco@hotmail.com>
2017-08-24 11:54:39 -0600

    UPDATE

--- views/site/signupm.php
+++ views/site/signupm.php
@@ -13,7 +13,9 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-signup">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to signup:</p>
+    <p>ASDASDASDASD:</p>
+
+    <p>otro cambio que hago</p>
 
     <div class="row">
         <div class="col-lg-5">
